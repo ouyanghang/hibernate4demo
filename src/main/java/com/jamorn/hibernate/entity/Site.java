dc680190aab9cea9cd2cd972406eaa0f3b455cbc
sunyamengbuhtig <sunyameng_anis@sina.com>
2014-04-02 17:55:40 +0800

    add sessionFactory

---
+++ src/main/java/com/jamorn/hibernate/entity/Site.java
@@ -0,0 +1,45 @@
+package com.jamorn.hibernate.entity;
+
+import java.util.Date;
+
+/**
+ * Created by sunyameng on 14-4-2.
+ */
+public class Site {
+    private Long id;
+    private String code;
+    private String name;
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/main/java/com/jamorn/hibernate/util/HibernateSessionFactory.java
@@ -0,0 +1,69 @@
+package com.jamorn.hibernate.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by sunyameng on 14-4-2.
+ */
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory;
+    private static final ThreadLocal<Session> threadLocal = new ThreadLocal<Session>();
+    private static String configFile = "/hibernate.cfg.xml";
+    static {
+        try {
+            Configuration configuration=new Configuration();
+            configuration.configure(configFile);
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            System.err.println("%%%% Error Creating SessionFactory %%%%");
+            e.printStackTrace();
+        }
+    }
+    private HibernateSessionFactory() {
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    public static Session getSession() throws HibernateException {
+        Session session =  threadLocal.get();
+        if (session == null || !session.isOpen()) {
+            if (sessionFactory == null) {
+                rebuildSessionFactory();
+            }
+            session = (sessionFactory != null) ? sessionFactory.openSession()
+                    : null;
+            threadLocal.set(session);
+        }
+
+        return session;
+    }
+    public static void closeSession() throws HibernateException {
+        Session session = threadLocal.get();
+        threadLocal.set(null);
+        if (session != null) {
+            session.close();
+        }
+    }
+
+    public static void rebuildSessionFactory() {
+        try {
+            Configuration configuration=new Configuration();
+            configuration.configure();
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            System.err.println("%%%% Error Creating SessionFactory %%%%");
+            e.printStackTrace();
+        }
+    }
+
+    public static void setConfigFile(String configFile) {
+        HibernateSessionFactory.configFile = configFile;
+        sessionFactory = null;
+    }
+}
